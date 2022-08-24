d88add5d6160641ae46df0e4c1b04ff7a7141ee6
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-08-24 18:21:24 +0300

    commit tree 23491b965f95f7717383a896e337368a816fcf5e

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,18 @@
 # Add a console message so we can see output when the seed file runs
+
+puts "Seeding the database..."
+
+# Seed the database with some dummy data
+
+
+50.times do
+
+    Game.create(
+        title: Faker::Game.name,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
+
+puts "Done seeding!"
